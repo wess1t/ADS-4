0e154825d53ec0828901fee2f95886ba20098f0c
Рыбина Екатерина <rybinae58@gmail.com>
2026-04-04 22:36:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,61 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int cnt = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1)
+      continue;
+    for (int j = i + 1; j < len; ++j) {
+      if (j > i + 1 && arr[j] == arr[j -1])
+        continue;
+      if (arr[i] + arr[j] == value) {
+        ++cnt;
+        break;
+      }
+    }
+  }
+  return cnt;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int cnt = 0, left = 0, right = len - 1;
+  while (left < right) {
+    int currentsum = arr[left] + arr[right];
+    if (currentsum == value) {
+      ++cnt;
+      int currentLeft = arr[left];
+      int currentRight = arr[right];
+      while (left < right && arr[left] == currentLeft)
+        ++left;
+      while (right > left && arr[right] == currentRight)
+        --right;
+    } else if (currentsum < value) {
+      ++left;
+    } else
+      --right;
+  }
+  return cnt;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int cnt = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1)
+      continue;
+    int k = value - arr[i];
+    if (k < arr[i])
+      continue;
+    int a = i + 1, b = len - 1;
+    bool found = false;
+    while (a <= b) {
+      int s = a + (b - a) / 2;
+      if (arr[s] == k) {
+        found = true;
+        break;
+      } else if (arr[s] < k) {
+        a = s + 1;
+      } else 
+        b = s - 1;
+    }
+    if (found) 
+      ++cnt;
+  }
+  return cnt;
 }
