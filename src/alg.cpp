e9d33970e2b689723bbfb8e38cb73f2dfb95163b
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-09 15:37:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,11 +18,16 @@ int countPairs2(int *arr, int len, int value) {
     while (l < r) {
         int sum = arr[l] + arr[r];
         if (sum == value) {
+            if (arr[l] == arr[r]) {
+                int n = r - l + 1;
+                cnt += n * (n - 1) / 2;
+                break;
+            }
             int l_cnt = 1;
-            int r_cnt = 1;
             while (l + l_cnt < r && arr[l + l_cnt] == arr[l]) {
                 l_cnt++;
             }
+            int r_cnt = 1;
             while (r - r_cnt > l && arr[r - r_cnt] == arr[r]) {
                 r_cnt++;
             }
@@ -45,28 +50,43 @@ int countPairs3(int *arr, int len, int value) {
             continue;
         }
         int target = value - arr[i];
+        if (target < arr[i]) {
+            continue;
+        }
         int l = i + 1;
         int r = len - 1;
+        int first = -1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (arr[mid] == target) {
-                int r_cnt = 1;
-                int temp_mid = mid;
-                while (temp_mid + 1 <= r && arr[temp_mid + 1] == target) {
-                    r_cnt++;
-                    temp_mid++;
-                }
-                temp_mid = mid;
-                while (temp_mid - 1 >= l && arr[temp_mid - 1] == target) {
-                    r_cnt++;
-                    temp_mid--;
-                }
-                cnt += r_cnt;
-                break;
+                first = mid;
+                r = mid - 1;
             } else if (arr[mid] < target) {
                 l = mid + 1;
             } else {
                 r = mid - 1;
+            }
+        }
+        if (first != -1) {
+            l = first;
+            r = len - 1;
+            int last = first;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (arr[mid] == target) {
+                    last = mid;
+                    l = mid + 1;
+                } else if (arr[mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            if (arr[i] == target) {
+                int n = last - first + 1;
+                cnt += n * (n - 1) / 2;
+            } else {
+                cnt += (last - first + 1);
             }
         }
     }
