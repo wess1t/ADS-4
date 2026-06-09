f1a62b62ea5f02d88e45847d0c1ab1541db975e1
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-09 16:18:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,9 @@
 int countPairs1(int *arr, int len, int value) {
     int cnt = 0;
     for (int i = 0; i < len; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
         for (int j = i + 1; j < len; ++j) {
-            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
             if (arr[i] + arr[j] == value) {
-                cnt++;
-                break;
+                ++cnt;
             }
         }
     }
@@ -16,15 +13,33 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
-        for (int j = len - 1; j > i; --j) {
-            if (j < len - 1 && arr[j] == arr[j + 1]) continue;
-            if (arr[i] + arr[j] == value) {
-                cnt++;
+    int l = 0;
+    int r = len - 1;
+    while (l < r) {
+        int sum = arr[l] + arr[r];
+        if (sum == value) {
+            int l_val = arr[l];
+            int r_val = arr[r];
+            if (l_val == r_val) {
+                int n = r - l + 1;
+                cnt += n * (n - 1) / 2;
                 break;
             }
-            if (arr[i] + arr[j] < value) break;
+            int l_cnt = 0;
+            while (l < r && arr[l] == l_val) {
+                l_cnt++;
+                l++;
+            }
+            int r_cnt = 0;
+            while (l <= r && arr[r] == r_val) {
+                r_cnt++;
+                r--;
+            }
+            cnt += l_cnt * r_cnt;
+        } else if (sum < value) {
+            l++;
+        } else {
+            r--;
         }
     }
     return cnt;
@@ -32,20 +47,22 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
     int cnt = 0;
-    int l = 0;
-    int r = len - 1;
-    while (l < r) {
-        int sum = arr[l] + arr[r];
-        if (sum == value) {
-            cnt++;
-            int l_val = arr[l];
-            int r_val = arr[r];
-            while (l < r && arr[l] == l_val) l++;
-            while (l <= r && arr[r] == r_val) r--;
-        } else if (sum < value) {
-            l++;
-        } else {
-            r--;
+    for (int i = 0; i < len; ++i) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
+        int target = value - arr[i];
+        if (target < arr[i]) continue;
+        int left = i + 1;
+        int right = len - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                cnt++;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
     }
     return cnt;
