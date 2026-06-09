014d14fd49fb6d52650d94c3aaf43a44e16b80ad
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-09 15:24:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1])
-            continue;
+    for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
-            if (j > i + 1 && arr[j] == arr[j - 1])
-                continue;
             if (arr[i] + arr[j] == value)
                 ++cnt;
         }
@@ -17,27 +13,30 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
     int cnt = 0, l = 0, r = len - 1;
     while (l < r) {
-        int currentsum = arr[l] + arr[r];
-        if (currentsum == value) {
-            int l_cnt = 1, r_cnt = 1;
-            while (l + l_cnt < r && arr[l + l_cnt] == arr[l])
-                l_cnt++;
-            while (r - r_cnt > l && arr[r - r_cnt] == arr[r])
-                r_cnt++;
-            
+        int sum = arr[l] + arr[r];
+        if (sum == value) {
             if (arr[l] == arr[r]) {
-                int total = l_cnt + r_cnt - 1;
-                cnt += total * (total - 1) / 2;
+                int n = r - l + 1;
+                cnt += n * (n - 1) / 2;
+                break;
             } else {
+                int l_cnt = 1, r_cnt = 1;
+                while (l + 1 < r && arr[l] == arr[l + 1]) {
+                    l_cnt++;
+                    l++;
+                }
+                while (r - 1 > l && arr[r] == arr[r - 1]) {
+                    r_cnt++;
+                    r--;
+                }
                 cnt += l_cnt * r_cnt;
+                l++;
+                r--;
             }
-            
-            l += l_cnt;
-            r -= r_cnt;
-        } else if (currentsum < value) {
-            ++l;
+        } else if (sum < value) {
+            l++;
         } else {
-            --r;
+            r--;
         }
     }
     return cnt;
@@ -45,21 +44,14 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1])
-            continue;
-        
+    for (int i = 0; i < len; ++i) {
         int target = value - arr[i];
-        if (target < arr[i])
-            continue;
-            
         int l = i + 1, r = len - 1;
-        int first_pos = -1;
-        
+        int first = -1, last = -1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (arr[mid] == target) {
-                first_pos = mid;
+                first = mid;
                 r = mid - 1;
             } else if (arr[mid] < target) {
                 l = mid + 1;
@@ -67,16 +59,13 @@ int countPairs3(int *arr, int len, int value) {
                 r = mid - 1;
             }
         }
-        
-        if (first_pos != -1) {
-            l = first_pos;
+        if (first != -1) {
+            l = first;
             r = len - 1;
-            int last_pos = first_pos;
-            
             while (l <= r) {
                 int mid = l + (r - l) / 2;
                 if (arr[mid] == target) {
-                    last_pos = mid;
+                    last = mid;
                     l = mid + 1;
                 } else if (arr[mid] < target) {
                     l = mid + 1;
@@ -84,14 +73,10 @@ int countPairs3(int *arr, int len, int value) {
                     r = mid - 1;
                 }
             }
-            
-            if (arr[i] == target) {
-                int count = last_pos - first_pos + 1;
-                cnt += count * (count - 1) / 2;
-            } else {
-                cnt += (last_pos - first_pos + 1);
-            }
+            cnt += (last - first + 1);
         }
+        while (i + 1 < len && arr[i] == arr[i + 1])
+            i++;
     }
     return cnt;
 }
